9b349e71fe8ddb7ad74211bf248eba1feae79385
Nazar Melnychuk <nazar.melnichuck@gmail.com>
2024-02-17 12:17:45 +0200

    Create UI

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.lab2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab2.ui.theme.Lab2Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +24,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab2Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    PageContent()
                 }
             }
         }
@@ -30,14 +33,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun PageContent() {
+    Column(
+        modifier = Modifier.padding(20.dp),
+    ) {
+        Text(text = "Page 1", modifier = Modifier.padding(bottom = 16.dp))
+        Row() {
+            Button(onClick = {}) {
+                Text(text = "Назад")
+            }
+            Button(
+                modifier = Modifier
+                    .padding(start = 16.dp),
+                onClick = {}) {
+                Text(text = "Вперед")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Lab2Theme {
-        Greeting("Android")
+        PageContent()
     }
 }
